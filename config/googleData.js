fdd24a55094d5d3ce62dc8eb9d588773bf8c70f0
Amriita <76736562+Amriita@users.noreply.github.com>
2022-02-26 14:08:18 +0530

    Update googleData.js

--- config/googleData.js
+++ config/googleData.js
@@ -1,2 +1,2 @@
-module.exports.clientId = "200976362234-i24ih0h8t3hvd5dqok7soiq6ugu8us19.apps.googleusercontent.com";
-module.exports.clientSecret = "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";
+module.exports.clientId = "clientId";
+module.exports.clientSecret = "Client Secret";
